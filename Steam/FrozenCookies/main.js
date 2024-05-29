29a7bed7dc932cc6669c6e7639a52254038b971a
arturino009 <51629394+arturino009@users.noreply.github.com>
2024-05-29 17:22:56 +0300

    Update main.js

--- Steam/FrozenCookies/main.js
+++ Steam/FrozenCookies/main.js
@@ -1,1 +1,1 @@
-Game.LoadMod("https://github.com/arturino009/FrozenCookies/blob/main/frozen_cookies.js");
+Game.LoadMod("https://raw.githubusercontent.com/arturino009/FrozenCookies/main/frozen_cookies.js");
