dbda728117b7b25fd3e767f9f8435d8f1d2edac1
arturino009 <51629394+arturino009@users.noreply.github.com>
2024-05-29 17:23:37 +0300

    Update fc_userscript_loader.user.js

--- fc_userscript_loader.user.js
+++ fc_userscript_loader.user.js
@@ -16,6 +16,6 @@ var loadInterval = setInterval(function () {
     const Game = unsafeWindow.Game;
     if (Game && Game.ready) {
         clearInterval(loadInterval);
-        Game.LoadMod("https://github.erbkaiser.com/FrozenCookies/frozen_cookies.js");
+        Game.LoadMod("https://raw.githubusercontent.com/arturino009/FrozenCookies/main/frozen_cookies.js");
     }
 }, 1000);
